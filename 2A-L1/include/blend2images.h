1f83ad5df50fb32fcff4a81fa569621a129e4e57
Pedro Velez <pdvelez@gatech.edu>
2017-05-08 12:58:08 -0500

    add header files

---
+++ 2A-L1/include/blend2images.h
@@ -0,0 +1,11 @@
+
+#ifndef __BLEND2IMAGES_H_INCLUDED__
+#define __BLEND2IMAGES_H_INCLUDED__
+
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+int blend2Images();
+
+#endif // __BLEND2IMAGES_H_INCLUDED__
---
+++ 2A-L1/include/colorPlanes.h
@@ -0,0 +1,12 @@
+
+#ifndef __COLORPLANES_H_INCLUDED__
+#define __COLORPLANES_H_INCLUDED__
+
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+int colorPlanes();
+
+#endif // __COLORPLANES_H_INCLUDED__
